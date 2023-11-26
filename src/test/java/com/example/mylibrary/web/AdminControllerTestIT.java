687fac10ba292962481b704220c2eaaaa6b20943
Savina Popova <savina.popova@yahoo.com>
2023-11-26 20:38:54 +0200

    more tests

--- src/test/java/com/example/mylibrary/web/AdminControllerTestIT.java
+++ src/test/java/com/example/mylibrary/web/AdminControllerTestIT.java
@@ -1,7 +1,10 @@
 package com.example.mylibrary.web;
 
+import com.example.mylibrary.model.dto.MessageDTO;
 import com.example.mylibrary.model.dto.UserDTO;
+import com.example.mylibrary.model.entity.Message;
 import com.example.mylibrary.model.entity.User;
+import com.example.mylibrary.repository.MessageRepository;
 import com.example.mylibrary.repository.UserRepository;
 import com.example.mylibrary.utils.TestUserData;
 import org.junit.jupiter.api.AfterEach;
@@ -34,22 +37,47 @@ class AdminControllerTestIT {
     private TestUserData testUserData;
 
     @Autowired
+    private MessageRepository messageRepository;
+
+    @Autowired
     private ModelMapper modelMapper;
 
     private User user;
 
     private User admin;
 
+    private Message openMessage1;
+
+    private Message openMessage2;
+
+    private Message openMessage3;
+
+    private Message closedMessage;
+
+
     @BeforeEach
     public void setUp() {
+        this.messageRepository.deleteAll();
         testUserData.cleanUp();
 
         user = testUserData.createTestUser();
         admin = testUserData.createTestAdmin();
+
+        openMessage1 = new Message("title1","question1");
+        openMessage1.setUser(user);
+        openMessage2 = new Message("title2","question2");
+        openMessage2.setUser(user);
+        openMessage3 = new Message("title3","question3");
+        openMessage3.setUser(admin);
+        closedMessage = new Message("title4","question4");
+        closedMessage.setUser(user);
+        closedMessage.setClosed(true);
+        closedMessage.setAdmin(admin);
     }
 
     @AfterEach
     public void tearDown() {
+        this.messageRepository.deleteAll();
         testUserData.cleanUp();
     }
 
@@ -82,6 +110,54 @@ class AdminControllerTestIT {
                 .andExpect(model().attribute("allUsers", List.of(userDTO)))
                 .andExpect(model().attribute("allUsers", contains(hasProperty("firstName", is("userFirstName")))))
                 .andExpect(view().name("people"))
+                .andExpect(forwardedUrl(null));
+
+    }
+
+    @Test
+    void testGetOpenMessagesWhenAnonymous() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/admin/messages"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("http://localhost/users/login"));
+    }
+
+    @Test
+    @WithUserDetails(value = "userEmail", userDetailsServiceBeanName = "userDetailsService", setupBefore = TestExecutionEvent.TEST_EXECUTION)
+    void testGetOpenMessagesWhenNotAdmin() throws Exception {
+
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/admin/messages"))
+                .andExpect(status().isForbidden());
+
+    }
+
+    @Test
+    @WithUserDetails(value = "adminEmail", userDetailsServiceBeanName = "userDetailsService", setupBefore = TestExecutionEvent.TEST_EXECUTION)
+    void testGetOpenMessagesWhenAdmin() throws Exception {
+
+
+        this.messageRepository.save(openMessage1);
+        this.messageRepository.save(openMessage2);
+        this.messageRepository.save(closedMessage);
+
+
+        List<Message> messages = this.messageRepository.findAllByClosed(false);
+
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/admin/messages"))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("messages", hasSize(2)))
+                .andExpect(model().attribute("messages", List.of(modelMapper.map(messages, MessageDTO[].class))))
+                .andExpect(model().attribute("messages", hasItem(
+                        allOf(
+                                hasProperty("title", is("title1"))
+
+                        )
+                )))
+                .andExpect(model().attribute("messages", hasItem(
+                        allOf(
+                                hasProperty("title", is("title2"))
+
+                        ))))
+                .andExpect(view().name("messages-admin"))
                 .andExpect(forwardedUrl(null));
 
     }
