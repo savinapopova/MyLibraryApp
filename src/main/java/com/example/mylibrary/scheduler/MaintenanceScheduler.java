bd2eea33ab63156b9303440f79c91b6f977c347d
Savina Popova <savina.popova@yahoo.com>
2023-12-01 23:43:04 +0200

    mylibrary

--- src/main/java/com/example/mylibrary/scheduler/MaintenanceScheduler.java
+++ src/main/java/com/example/mylibrary/scheduler/MaintenanceScheduler.java
@@ -15,23 +15,17 @@ public class MaintenanceScheduler {
         this.maintenanceInterceptor = maintenanceInterceptor;
     }
 
-    @Scheduled(cron = "0 3 9 * * THU") // Крон израз за неделя в 03:00
+    @Scheduled(cron = "0 3 9 * * THU")
     public void activateMaintenance() {
         System.out.println("MAINTENANCE ON");
         maintenanceInterceptor.activateMaintenanceMode();
     }
 
-    @Scheduled(cron = "0 5 9 * * THU") // Крон израз за неделя в 03:20
+    @Scheduled(cron = "0 5 9 * * THU")
     public void deactivateMaintenance() {
         System.out.println("MAINTENANCE OFF");
         maintenanceInterceptor.deactivateMaintenanceMode();
     }
 
-//    @Scheduled(cron = "*/10 * * * * *")
-////    @Scheduled(fixedRate = 10_000)
-//    public void cleanUp() {
-//        maintenanceInterceptor.activateMaintenanceMode();
-//        System.out.println("Trigger cleanup of activation links. " + LocalDateTime.now());
-//
-//    }
+
 }
